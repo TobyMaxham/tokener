71ff691deee88e3794922bae65ee66dd450e72e9
TobyMaxham <tobias.maxham@tricept.de>
2015-09-04 17:51:08 +0200

    ea

--- src/Build/Collection.php
+++ src/Build/Collection.php
@@ -1,1 +1,13 @@
 <?php
+
+namespace TobyMaxham\Tokener\Build;
+
+/**
+ * Class Collection
+ * @package TobyMaxham\Tokener\Build
+ * @author Tobias Maxham <git2015@maxham.de>
+ */
+class Collection
+{
+
+}
